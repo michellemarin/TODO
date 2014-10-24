e8e5978d0963b4a982498671ae5bfee01913b22f
Michelle Marin <michelle@carinocreative.com>
2014-10-24 10:54:15 -0400

    hope this works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,12 +7,31 @@ var todoTemplate   = _.template( $('#todo-template').html() );
 var footerTemplate = _.template( $('#footer-template').html() );
 // Don't remove anything above this line ----------------------- //
 
+var Todo = function (todoData) {
+  var defaults = {
+    id: new Date().getTime(),
+    title: 'no title',
+    completed: '',
+    checked: ''
+  };
+
+  return _.extend(defaults, todoData);
+};
+
+$('#new-todo').keypress(function(e) {
+  if(e.which == 13) {
+    $('#todo-list').append(todoTemplate(new Todo({
+      title: $(this).val()
+    })));
+  }
+});
 
 //These are examples, please remove and replace with your own code
 $('#todo-list').append(todoTemplate({
     id: 1,
     title: 'Finish Todo',
     completed: 'completed',
+    checked: 'checked'
 }));
 
 
